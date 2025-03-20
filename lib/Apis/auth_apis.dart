0516c688d99648b68d4070c5ef5c3d775c9eeaa3
Rehmanali <rehamanali.zakirhussain@akshayakalpa.org>
2025-03-20 19:06:12 +0530

    20-03-2025 update

--- lib/Apis/auth_apis.dart
+++ lib/Apis/auth_apis.dart
@@ -121,6 +121,16 @@ class AuthApis {
     }
   }
 
+  static Future<dio.Response?> addProductAPI(dio.FormData body) async {
+    dio.Response res = await formService.postRequest(
+        "https://raxaspread.com/API/api/add_product", body, null, null);
+    if (res.statusCode! >= 200 && res.statusCode! <= 210) {
+      return res;
+    } else if (res.statusCode! >= 400 && res.statusCode! <= 403) {
+      return null;
+    }
+  }
+
   static Future<dio.Response?> deleteOrderApi(int id) async {
     dio.Response res = await formService.deleteRequest(
         "https://raxaspread.com/API/api/delete_product/${id}", null);
